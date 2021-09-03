6bf8be8767faccb5157bba140befdf8ba95c6bc8
Christian Jess Mark <cjess@dfds.com>
2021-09-03 21:51:29 +0200

    refactor: refine list query, add pagination vars

--- src/PokemonList.tsx
+++ src/PokemonList.tsx
@@ -1,9 +1,15 @@
-import { usePokemonQuery } from './generated/graphql'
+import { usePokemonListQuery } from './generated/graphql'
 
 function PokemonList() {
-    const { data } = usePokemonQuery()
-
-    return <div>{data}</div>
+    const { data } = usePokemonListQuery({ limit: 10, offset: 0 })
+    console.log(`data`, data)
+    return (
+        <div>
+            {data?.pokemon_v2_pokemon?.map((pokemon) => (
+                <p>{pokemon.name}</p>
+            ))}
+        </div>
+    )
 }
 
 export default PokemonList
