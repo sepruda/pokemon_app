27a1ea78b15afa47da9d093ae1b2e99eb16e8319
Christian Jess Mark <cjess@dfds.com>
2021-09-20 16:28:30 +0200

    style: initial grid setup

--- src/PokemonList.tsx
+++ src/PokemonList.tsx
@@ -1,23 +1,53 @@
+import styled from '@emotion/styled/macro'
 import { usePokemonListQuery } from './generated/graphql'
+
+const Wrapper = styled.ul`
+    display: grid;
+    grid-template-columns: repeat(auto-fill, minmax(200px, 1fr));
+    grid-gap: 16px;
+`
+
+const Card = styled.li`
+    max-width: 300px;
+    border-radius: 5px;
+    background-color: lightgrey;
+    list-style-type: none;
+    padding: 8px;
+    display: flex;
+    flex-direction: column;
+`
+
+const CardTitle = styled.h3`
+    text-align: center;
+    text-transform: capitalize;
+`
+
+const CardImage = styled.img`
+    align-self: center;
+`
 
 function PokemonList() {
     const { data } = usePokemonListQuery({ limit: 25, offset: 0 })
     console.log(`data`, data)
     return (
-        <ul>
+        <Wrapper>
             {data?.pokemon_v2_pokemon?.map((pokemon) => {
-                const { id, name } = pokemon
+                const { id, name, weight, height } = pokemon
                 return (
-                    <li key={id}>
-                        {name}
-                        <img
+                    <Card key={id}>
+                        <CardTitle>{name}</CardTitle>
+                        <CardImage
                             src={`${process.env.PUBLIC_URL}/pokemons/${id}.png`}
                             alt={name}
                         />
-                    </li>
+                        <div>
+                            <p>{`Weight - ${weight}`}</p>
+                            <p>{`Height - ${height}`}</p>
+                        </div>
+                    </Card>
                 )
             })}
-        </ul>
+        </Wrapper>
     )
 }
 
